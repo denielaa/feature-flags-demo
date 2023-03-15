1f3ec5d623e9eca1d52d2671de97f59efe0df6b3
Deniel Ariesta <deniel.ariesta@gmail.com>
2023-03-15 15:15:42 +0700

    activate boolean first

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,17 +2,17 @@ import './styles.css'
 
 import { FlagProvider } from '@unleash/proxy-client-react'
 
-// import { PricingTableBoolean } from '~/shared/components/PricingTable'
-import { PricingTableUserId } from '~/shared/components/PricingTable/PricingTableUserId'
+import { PricingTableBoolean } from '~/shared/components/PricingTable'
+// import { PricingTableUserId } from '~/shared/components/PricingTable/PricingTableUserId'
 import { config as UnleashConfig } from '~/shared/configs/unleash'
 
 function App() {
   return (
     <FlagProvider config={UnleashConfig}>
       <div className="flex justify-center mt-10">
-        {/* <PricingTableBoolean /> */}
+        <PricingTableBoolean />
 
-        <PricingTableUserId />
+        {/* <PricingTableUserId /> */}
       </div>
     </FlagProvider>
   )
